7e12cbe235a2af8515c861981fd749734d0f2795
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-01 14:51:50 +0500

    WhoIsOn component structure defined

---
+++ packages/features/components/WhoIsOn/container/WhoIsOn.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from '../styles/WhoIsOn.styles';
+
+const WhoIsOn = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Who Is On </Text>
+    </View>
+  );
+};
+
+export default WhoIsOn;
