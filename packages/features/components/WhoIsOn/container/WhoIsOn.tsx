428481120e33153e5f30fe3f1680660289b5334a
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-01 16:38:49 +0500

    component UI structure complete

--- packages/features/components/WhoIsOn/container/WhoIsOn.tsx
+++ packages/features/components/WhoIsOn/container/WhoIsOn.tsx
@@ -1,10 +1,49 @@
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from '../styles/WhoIsOn.styles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import COMMON_CONSTANTS from '../../../../constants/CommonConstants';
+import { COLORS } from '../../../../constants/theme';
 
 const WhoIsOn = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Who Is On </Text>
+      <View style={styles.widgetHeaderContainer}>
+        <Ionicons
+          name='time-outline'
+          style={styles.iconStyle}
+          size={COMMON_CONSTANTS.SIZE.SIZE_24}
+          color={COLORS.CLOCKWISE_PRIMARY}
+        />
+        <Text style={styles.title}> Who Is On Now </Text>
+      </View>
+
+      <View style={styles.widgetListContainer}>
+        <View style={styles.widgetListItem}>
+          <View style={styles.avatarIconContainer}>
+            <Text style={styles.avatarText}>JD</Text>
+          </View>
+
+          <View style={styles.nameContainer}>
+            <Text style={styles.nameText}>John Doe</Text>
+          </View>
+        </View>
+
+        <View style={styles.widgetListItem}>
+          <View style={styles.avatarIconContainer}>
+            <Text style={styles.avatarText}>JD</Text>
+          </View>
+
+          <View style={styles.nameContainer}>
+            <Text style={styles.nameText}>John Doe</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.bottomBorder}>
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.bottomText}>SEE MORE</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- packages/features/components/WhoIsOn/styles/WhoIsOn.styles.ts
+++ packages/features/components/WhoIsOn/styles/WhoIsOn.styles.ts
@@ -1,16 +1,77 @@
 import { Appearance, StyleSheet } from 'react-native';
+import { COLORS, FONTS, MODE } from '../../../../constants/theme';
 
 const mode = Appearance.getColorScheme();
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+  widgetHeaderContainer: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    paddingLeft: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 0.6,
+    borderBottomColor:
+      mode === MODE.DARK
+        ? COLORS.BORDER_COLOR_DARK_MODE
+        : COLORS.BORDER_COLOR_LIGHT_MODE,
+  },
+  widgetListItem: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 12,
+  },
+  avatarIconContainer: {
+    borderRadius: 50,
+    height: 40,
+    width: 40,
+    backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  widgetListContainer: {},
+  avatarText: {
+    fontSize: 17,
+    color: COLORS.WHITE,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+  },
+  nameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  nameText: {
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+    fontSize: 16,
+    color: mode === MODE.DARK ? COLORS.TEXT_DARK_MODE : COLORS.TEXT_LIGHT_MODE,
+  },
+  iconStyle: {
+    paddingTop: 5,
+  },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 21,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
     color: mode === 'dark' ? '#fff' : '#000',
+  },
+  bottomBorder: {
+    paddingVertical: 15,
+    marginTop: 10,
+    alignItems: 'center',
+    borderTopWidth: 0.6,
+    borderTopColor:
+      mode === MODE.DARK
+        ? COLORS.BORDER_COLOR_DARK_MODE
+        : COLORS.BORDER_COLOR_LIGHT_MODE,
+  },
+  bottomText: {
+    fontSize: 18,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+    color: COLORS.CLOCKWISE_PRIMARY,
   },
 });
 
