386f9ca17a1d4ec9c454ef6332d4f01a268c093a
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-13 16:02:59 +0500

    splash screen constants shifted

--- packages/features/SplashScreen/styles/SplashScreen.styles.ts
+++ packages/features/SplashScreen/styles/SplashScreen.styles.ts
@@ -1,32 +1,33 @@
 import { Appearance, StyleSheet } from 'react-native';
 import { COLORS, MODE, FONT_SIZE, FONTS } from '../../../constants/theme';
+import COMMON_CONSTANTS from '../../../constants/CommonConstants';
 
 const mode = Appearance.getColorScheme();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: COMMON_CONSTANTS.FLEX.CENTER,
+    alignItems: COMMON_CONSTANTS.FLEX.CENTER,
     backgroundColor:
       mode === MODE.DARK
         ? COLORS.BACKGROUND_DARK_MODE
         : COLORS.BACKGROUND_LIGHT_MODE,
   },
   logoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: COMMON_CONSTANTS.FLEX.ROW,
+    alignItems: COMMON_CONSTANTS.FLEX.CENTER,
   },
   logo: {
     fontSize: FONT_SIZE.SIZE_50,
     color: COLORS.CLOCKWISE_PRIMARY,
     fontFamily: FONTS.CLOCKWISE_REGULAR,
-    marginRight: 8,
+    marginRight: COMMON_CONSTANTS.SIZE.SIZE_8,
   },
   logoSeparator: {
     fontSize: FONT_SIZE.SIZE_50,
     color: COLORS.WHITE,
     fontFamily: FONTS.CLOCKWISE_REGULAR,
-    marginRight: 8,
+    marginRight: COMMON_CONSTANTS.SIZE.SIZE_8,
   },
   logoText: {
     fontSize: FONT_SIZE.SIZE_50,
