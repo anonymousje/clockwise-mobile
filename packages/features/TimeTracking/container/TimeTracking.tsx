cb38c7aa803fbc4e10ae6f43402926c11f6d25f3
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-20 15:18:04 +0500

    Structure created

--- packages/features/TimeTracking/container/TimeTracking.tsx
+++ packages/features/TimeTracking/container/TimeTracking.tsx
@@ -7,6 +7,36 @@ const TimeTracking = () => {
       <View style={styles.header}>
         <Text style={styles.headerText}>Time Tracking</Text>
       </View>
+      <View
+        style={{
+          borderWidth: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <View style={{ borderWidth: 1, height: 100, width: '20%' }}>
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <Text style={{ fontSize: 16 }}>Tue</Text>
+            <Text style={{ fontSize: 25 }}>12</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            justifyContent: 'center',
+            borderWidth: 1,
+            height: 100,
+            width: '80%',
+            paddingLeft: 10,
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>Intern Sandbox</Text>
+          <Text style={{ fontSize: 16 }}>12:00 - 13:00</Text>
+          <Text style={{ fontSize: 14 }}>Butcher</Text>
+          <Text style={{ fontSize: 16 }}>Pending</Text>
+        </View>
+      </View>
     </View>
   );
 };
