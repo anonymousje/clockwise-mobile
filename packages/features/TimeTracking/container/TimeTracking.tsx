24bdf2e65c04901ceef1e6186491f64479085413
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-21 18:21:48 +0500

    UI improved

--- packages/features/TimeTracking/container/TimeTracking.tsx
+++ packages/features/TimeTracking/container/TimeTracking.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../styles/TimeTracking.styles';
 
 const TimeTracking = () => {
@@ -7,34 +7,37 @@ const TimeTracking = () => {
       <View style={styles.header}>
         <Text style={styles.headerText}>Time Tracking</Text>
       </View>
-      <View
-        style={{
-          borderWidth: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
-      >
-        <View style={{ borderWidth: 1, height: 100, width: '20%' }}>
-          <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-          >
-            <Text style={{ fontSize: 16 }}>Tue</Text>
-            <Text style={{ fontSize: 25 }}>12</Text>
+
+      <View style={styles.content}>
+        <View style={styles.timeEntryCard}>
+          <View style={styles.dateSection}>
+            <Text style={styles.dayText}>TUE</Text>
+            <Text style={styles.dateText}>19</Text>
+          </View>
+
+          <View style={styles.detailsSection}>
+            <View style={styles.titleRow}>
+              <Text style={styles.titleText}>intern_sandbox</Text>
+              <Text style={styles.timeText}>7:15 AM - 7:15 AM</Text>
+              <Text style={styles.roleText}>Butcher</Text>
+              <Text style={styles.statusText}>Pending • 16s</Text>
+            </View>
+
+            <Text style={styles.approveText}>APPROVE</Text>
           </View>
         </View>
-        <View
-          style={{
-            justifyContent: 'center',
-            borderWidth: 1,
-            height: 100,
-            width: '80%',
-            paddingLeft: 10,
-          }}
-        >
-          <Text style={{ fontSize: 20 }}>Intern Sandbox</Text>
-          <Text style={{ fontSize: 16 }}>12:00 - 13:00</Text>
-          <Text style={{ fontSize: 14 }}>Butcher</Text>
-          <Text style={{ fontSize: 16 }}>Pending</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={styles.approveButton}
+          >
+            <Text style={styles.buttonText}>Approve</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.unapproveButton}>
+            <Text style={styles.buttonText}>Unapprove</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
--- packages/features/TimeTracking/styles/TimeTracking.styles.ts
+++ packages/features/TimeTracking/styles/TimeTracking.styles.ts
@@ -1,14 +1,12 @@
 import { Appearance, StyleSheet } from 'react-native';
-import { COLORS, FONTS, MODE } from '../../../constants/theme';
+import { COLORS, FONTS, FONT_SIZE, MODE } from '../../../constants/theme';
+import COMMON_CONSTANTS from '../../../constants/CommonConstants';
 
 const mode = Appearance.getColorScheme();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:
-      mode === MODE.DARK
-        ? COLORS.BACKGROUND_DARK_MODE
-        : COLORS.BACKGROUND_LIGHT_MODE,
+    backgroundColor: COLORS.BACKGROUND_DARKER_MODE,
     paddingVertical: 5,
   },
   header: {
@@ -22,6 +20,105 @@ const styles = StyleSheet.create({
     color: mode === MODE.DARK ? COLORS.TEXT_DARK_MODE : COLORS.TEXT_LIGHT_MODE,
     fontSize: 20,
     fontFamily: FONTS.CLOCKWISE_BOLD,
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  timeEntryCard: {
+    flexDirection: 'row',
+    backgroundColor: COLORS.BACKGROUND_DARK_MODE,
+    borderRadius: 8,
+    marginBottom: 20,
+    overflow: 'hidden',
+  },
+  dateSection: {
+    width: '20%',
+    marginVertical: 20,
+    alignItems: 'center',
+    borderRightWidth: 3,
+    borderColor: '#F39C12',
+    justifyContent: 'center',
+    paddingVertical: 20,
+  },
+  dayText: {
+    color: COLORS.WHITE,
+    fontSize: FONT_SIZE.SIZE_15,
+    fontFamily: FONTS.CLOCKWISE_BOLD,
+  },
+  dateText: {
+    color: COLORS.WHITE,
+    fontSize: FONT_SIZE.SIZE_30,
+    fontFamily: FONTS.CLOCKWISE_BOLD,
+  },
+  detailsSection: {
+    flex: 1,
+    padding: 15,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  titleRow: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: COLORS.TEXT_DARK_MODE,
+    fontSize: FONT_SIZE.SIZE_20,
+    fontFamily: FONTS.CLOCKWISE_BOLD,
+    marginBottom: 8,
+  },
+  warningIcon: {
+    marginRight: 10,
+  },
+  approveText: {
+    color: COLORS.GREEN,
+    fontSize: FONT_SIZE.SIZE_15,
+    fontFamily: FONTS.CLOCKWISE_BOLD,
+    alignSelf: 'center',
+  },
+  timeText: {
+    color: COLORS.TEXT_DARK_MODE,
+    fontSize: FONT_SIZE.SIZE_17,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+    marginBottom: 4,
+  },
+  roleText: {
+    color: COLORS.TEXT_DARK_MODE,
+    fontSize: FONT_SIZE.SIZE_15,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+    marginBottom: 4,
+  },
+  statusText: {
+    color: '#F39C12',
+    fontSize: FONT_SIZE.SIZE_15,
+    fontFamily: FONTS.CLOCKWISE_REGULAR,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+
+    gap: COMMON_CONSTANTS.SIZE.SIZE_10,
+    paddingVertical: COMMON_CONSTANTS.SIZE.SIZE_5,
+    justifyContent: 'center',
+    marginTop: 'auto',
+  },
+  unapproveButton: {
+    width: '50%',
+    backgroundColor: COLORS.CLOCK_OUT_BUTTON_BACKGROUND,
+    alignItems: 'center',
+    paddingVertical: COMMON_CONSTANTS.SIZE.SIZE_12,
+    borderRadius: COMMON_CONSTANTS.SIZE.SIZE_5,
+  },
+  approveButton: {
+    width: '50%',
+    backgroundColor: COLORS.BREAK_BUTTON_BACKGROUND,
+    alignItems: 'center',
+    paddingVertical: COMMON_CONSTANTS.SIZE.SIZE_12,
+    borderRadius: COMMON_CONSTANTS.SIZE.SIZE_5,
+  },
+  buttonText: {
+    fontFamily: FONTS.CLOCKWISE_BOLD,
+    fontSize: FONT_SIZE.SIZE_17,
+    color: 'white',
   },
 });
 
