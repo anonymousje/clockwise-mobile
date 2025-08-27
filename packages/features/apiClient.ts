937cdf58221120516bacd55c30cb93e8ea9164f4
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-27 19:27:02 +0500

    refactor/ready for PR

--- packages/features/apiClient.ts
+++ packages/features/apiClient.ts
@@ -19,7 +19,6 @@ apiClient.interceptors.request.use(
     if (accessToken) {
       config.headers.Cookie = `${COMMON_CONSTANTS.API_HEADERS.JWT}${accessToken}`;
     }
-    console.log('API Request:', config);
     return config;
   },
   function (error) {
@@ -29,7 +28,6 @@ apiClient.interceptors.request.use(
 
 apiClient.interceptors.response.use(
   function (response) {
-    console.log('API Response:', response);
     return response;
   },
 
