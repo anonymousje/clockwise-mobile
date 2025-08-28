c258a4cdab2c56a459f8e9f38993800f0bcc2212
Foad Ahmed <foadahmed0903@gmail.com>
2025-08-28 18:52:51 +0500

    logs removed

--- packages/features/apiClient.ts
+++ packages/features/apiClient.ts
@@ -19,7 +19,6 @@ apiClient.interceptors.request.use(
     if (accessToken) {
       config.headers.Cookie = `${COMMON_CONSTANTS.API_HEADERS.JWT}${accessToken}`;
     }
-    console.log('Request Config:', config);
     return config;
   },
   function (error) {
@@ -29,7 +28,6 @@ apiClient.interceptors.request.use(
 
 apiClient.interceptors.response.use(
   function (response) {
-    console.log('Response:', response);
     return response;
   },
 
